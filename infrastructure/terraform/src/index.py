949ebdbbfa70327b2a4b69c3a836722326e02363
44103 <universeengineer@gmail.com>
2020-07-07 11:44:20 +0900

    Lambdaのリファクタリング

--- infrastructure/terraform/src/index.py
+++ infrastructure/terraform/src/index.py
@@ -1,10 +1,16 @@
 from trello import TrelloClient
-import json
-from datetime import datetime, timedelta
 import os
-import base64
 
 import trevent as te
+import scrum as sc
+
+def return_context(message):
+  return {
+    'isBase64Encoded': False,
+    'statusCode': 200,
+    'headers': {},
+    'body': f'{{"message": "{message}"}}'
+  }
 
 def lambda_handler(event, context):
   # Trello Client
@@ -14,56 +20,17 @@ def lambda_handler(event, context):
   )
   
   try:
-    board = client.get_board(os.environ["trello_board_id"])
-    dic_label = {}
-    for label in board.get_labels():
-      dic_label[label.name] = label
-    target_comments = ["開始時間", "中断時間", "完了時間"]
-    delta_time = timedelta()
-    tmp_time = datetime(year=2000, month=6, day=15)
     # イベント管理クラス
     tr_event = te.TrelloEvent(event)
-    # 自動打刻
+    # デバッグ用
     target_card = client.get_card(tr_event.get_card_id())
-    list_before = tr_event.get_list_before_name()
-    list_after = tr_event.get_list_after_name()
-    if "ToDo" in list_before and "Doing" in list_after:
-      target_card.comment(list_before + "->" + list_after +
-      "\n開始時間：" + datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
-    if "Doing" in list_before:
-      if "ToDo" in list_after:
-        target_card.comment(list_before + "->" + list_after +
-        "\n中断時間：" + datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
-      elif "Done" in list_after:
-        target_card.comment(list_before + "->" + list_after +
-        "\n完了時間：" + datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
-        # 2H超えのタスクにタグをつける
-        for c in target_card.get_comments():
-          for tg_c in target_comments:
-            if tg_c in c["data"]["text"]:
-              break
-          else:
-            continue
-          comment = c['data']['text'].split('\n')[1].split('：')
-          if "開始時間" in comment[0]:
-            delta_time += (tmp_time - datetime.strptime(comment[1], '%Y/%m/%d %H:%M:%S'))
-          else:
-            delta_time -= (tmp_time - datetime.strptime(comment[1], '%Y/%m/%d %H:%M:%S'))
-        if delta_time > timedelta(hours=2):
-          target_card.add_label(dic_label["2H"])
+    # Scrumに必要な作業を管理
+    scrum = sc.Scrum(client, tr_event)
+    scrum.stamping()
 
   except Exception as e:
-    # target_card.comment(f"{e}")
-    return {
-      'isBase64Encoded': False,
-      'statusCode': 200,
-      'headers': {},
-      'body': f'{{"message": "{e}"}}'
-    }
+    # デバッグ用
+    target_card.comment(f"{e}")
+    return return_context(f"{e}")
 
-  return {
-    'isBase64Encoded': False,
-    'statusCode': 200,
-    'headers': {},
-    'body': '{"message": "Succeed: Normal Exit"}'
-  }
+  return return_context("Succeed: Normal Exit")
